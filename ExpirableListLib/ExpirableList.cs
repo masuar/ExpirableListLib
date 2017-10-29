fc4c999c7eadec08fd544b03ff27c9121415ce3d
masuar <masuar@hotmail.com>
2017-10-29 22:45:08 +0100

    initial

---
+++ ExpirableListLib/ExpirableList.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ExpirableListLib
+{
+    public class ExpirableList<T>
+    {
+        private int timeoutInMs;
+        private List<T> items;
+        private Timer timer;
+
+        public event ListFinishedEventHandler<T> ListFinished;   
+
+        public ExpirableList(int timeoutInMs, int capacity)
+        {
+            this.items = new List<T>(capacity);
+            this.timer = new Timer(timeoutInMs);
+            this.timer.Elapsed += Timer_Elapsed;
+        }
+
+        public void Add(T item)
+        {
+            if (this.items.Count == 0)
+            {
+                this.timer.Start();
+            }
+
+            if (this.timer.Enabled)
+            {
+                this.items.Add(item);
+
+                if (this.items.Count == this.items.Capacity)
+                {
+                    this.timer.Stop();
+                    if (this.ListFinished != null)
+                    {
+                        this.ListFinished(this, new ListFinishedEventArgs<T>(this.items, true));
+                    }
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot add items while list stopped.");
+            }
+        }
+
+        public IEnumerable<T> Items
+        {
+            get
+            {
+                return this.items;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.items.Capacity;
+            }
+        }
+
+        public bool IsStopped
+        {
+            get
+            {
+                return !this.timer.Enabled;
+            }
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            this.timer.Stop();
+
+            if (this.ListFinished != null)
+            {
+                this.ListFinished(this, new ListFinishedEventArgs<T>(this.items, this.items.Count == this.items.Capacity));
+                this.ListFinished = null;
+            }
+        }
+    }
+
+    public class ListFinishedEventArgs<T> : EventArgs
+    {
+                
+        public ListFinishedEventArgs(IEnumerable<T> items, bool isListComplete)
+        {
+            this.Items = items;
+            this.IsListComplete = isListComplete;
+        }
+
+        public IEnumerable<T> Items { get; private set; }
+
+        public bool IsListComplete { get; private set; }
+    }
+
+    public delegate void ListFinishedEventHandler<T>(ExpirableList<T> sender, ListFinishedEventArgs<T> e);
+}
---
+++ ExpirableListTests/ExpirableListTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExpirableListLib;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpirableListTests
+{
+    [TestClass]
+    public class ExpirableListTests
+    {
+        [TestMethod]
+        public void CompleteListBeforeExpiracy_RaiseFinishCompletedEvent()
+        {
+            // Arrange
+            bool raised = false;
+            ExpirableList<string> expirableList = new ExpirableList<string>(500, 3);
+            expirableList.ListFinished += (sender, e) => 
+            {
+                Assert.IsTrue(e.IsListComplete);
+                Assert.AreEqual(3, e.Items.Count());
+                raised = true;
+            };
+
+            expirableList.Add("item1");
+            expirableList.Add("item2");
+            expirableList.Add("item3");
+
+            // Execute
+            System.Threading.Thread.Sleep(600);
+
+            // Assert
+            Assert.IsTrue(raised);
+            Assert.IsTrue(expirableList.IsStopped);
+        }
+
+        [TestMethod]
+        public void ExpireListBeforeComplete_RaiseFinishIncompletedEvent()
+        {
+            // Arrange
+            bool raised = false;
+            ExpirableList<string> expirableList = new ExpirableList<string>(500, 3);
+            expirableList.ListFinished += (sender, e) => 
+            {
+                Assert.IsFalse(e.IsListComplete);
+                Assert.AreEqual(2, e.Items.Count());
+                raised = true;
+            };
+
+            expirableList.Add("item1");
+            expirableList.Add("item2");
+
+            // Execute
+            System.Threading.Thread.Sleep(600);
+
+            // Assert
+            Assert.IsTrue(raised);
+            Assert.IsTrue(expirableList.IsStopped);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void AddingMoreItems_ThrowException()
+        {
+            // Arrange
+            bool complete = false;
+            ExpirableList<string> expirableList = new ExpirableList<string>(500, 3);
+            expirableList.ListFinished += (sender, e) => { complete = e.IsListComplete; };
+            expirableList.Add("item1");
+            expirableList.Add("item2");
+            expirableList.Add("item3");
+            expirableList.Add("item4");
+
+            // Execute
+            System.Threading.Thread.Sleep(500);
+        }
+
+    }
+}
